5c1df4d10ba2211dc0e71ed088552f68a5dda17c
sofush <102423577+sofush@users.noreply.github.com>
2025-02-28 20:48:15 +0100

    feat: add logging to rust benchmark server

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 use bitmap_image_header::BitmapImageHeader;
+use log::{debug, error, info, trace};
 
 mod bitmap_image_header;
 
@@ -18,6 +19,7 @@ fn convert_to_greyscale<S: Read + Write>(
     let mut pixel_buffer = vec![0u8; row_width];
 
     for _row in 0..bmp.height() {
+        trace!("Reading {row_width} bytes into pixel buffer.");
         reader.read_exact(&mut pixel_buffer)?;
 
         for col in (0..row_width as usize).step_by(3) {
@@ -30,14 +32,18 @@ fn convert_to_greyscale<S: Read + Write>(
             }
         }
 
+        trace!("Writing {row_width} greyscale bytes back to client.");
         writer.write_all(&pixel_buffer)?;
 
         if padding > 0 {
+            trace!("Reading {padding} bytes of padding.");
             reader.read_exact(&mut padding_buffer)?;
+            trace!("Writing {padding} bytes of padding.");
             writer.write_all(&padding_buffer)?;
         }
     }
 
+    debug!("Conversion to greyscale succeeded.");
     Ok(())
 }
 
@@ -45,7 +51,8 @@ fn handle_connection<S: Read + Write>(
     mut reader: BufReader<S>,
     mut writer: BufWriter<S>,
 ) -> Result<(), String> {
-    // Læs header på bitmap billede-fil (.bmp)
+    debug!("Parsing BMP header.");
+
     let bmp = match BitmapImageHeader::try_from(&mut reader) {
         Ok(i) => i,
         Err(e) => {
@@ -67,17 +74,18 @@ fn handle_connection<S: Read + Write>(
         dbg!(bmp.offset());
     }
 
-    // Send header tilbage til klienten.
+    debug!("Writing BMP header.");
+
     if let Err(e) = bmp.write_to(&mut writer) {
         return Err(format!("Could not write header to client: {e}"));
     }
 
-    // Konverter hver pixel til gråtoner.
+    debug!("Converting pixels to greyscale.");
+
     if let Err(e) = convert_to_greyscale(&mut reader, &mut writer, &bmp) {
         return Err(format!("Could not convert BMP to greyscale: {e}"));
     }
 
-    // Send resten af filen tilbage klienten.
     let mut buffer = [0u8; 8192];
 
     loop {
@@ -86,6 +94,7 @@ fn handle_connection<S: Read + Write>(
                 break;
             }
             Ok(n) => {
+                debug!("Writing {n} bytes from tail of stream.");
                 if let Err(e) = writer.write_all(&buffer[..n]) {
                     return Err(format!("Could not write to TCP stream: {e}"));
                 }
@@ -104,22 +113,24 @@ fn handle_connection<S: Read + Write>(
 }
 
 fn main() {
+    env_logger::init();
+
     let server = match TcpListener::bind("127.0.0.1:8081") {
         Ok(s) => s,
         Err(e) => {
-            eprintln!("Could not create TcpListener: {e}");
+            error!("Could not create TcpListener: {e}");
             return;
         }
     };
 
     loop {
-        println!("Waiting for client to connect...");
+        info!("Waiting for client to connect...");
 
         match server.accept() {
             Ok((stream, _)) => {
                 let writer = {
                     let Ok(stream_clone) = stream.try_clone() else {
-                        eprintln!("Could not clone TCP stream.");
+                        error!("Could not clone TCP stream.");
                         return;
                     };
 
@@ -127,21 +138,21 @@ fn main() {
                 };
                 let reader = {
                     let Ok(stream_clone) = stream.try_clone() else {
-                        eprintln!("Could not clone TCP stream.");
+                        error!("Could not clone TCP stream.");
                         return;
                     };
                     BufReader::with_capacity(65_536, stream_clone)
                 };
 
                 if let Err(e) = handle_connection(reader, writer) {
-                    eprintln!("Error: {e}");
+                    error!("Error: {e}");
                 }
 
                 if let Err(e) = stream.shutdown(Shutdown::Both) {
-                    eprintln!("Could not shutdown stream: {e}");
+                    error!("Could not shutdown stream: {e}");
                 }
             }
-            Err(e) => println!("Could not get client: {e}"),
+            Err(e) => error!("Could not get client: {e}"),
         }
     }
 }
